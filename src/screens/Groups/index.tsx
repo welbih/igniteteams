23caca3c603a11794705ab7c026ac1db49ac4876
welber.fernandes <welbih.ti@gmail.com>
2024-05-03 16:02:19 -0300

    Created component Header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
-
 import { Container } from './styles';
+
+import { Header } from '@components/Header';
 
 export function Groups() {
   return (
     <Container>
-
+      <Header />
     </Container>
   );
 }
