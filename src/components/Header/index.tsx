881499aa5d54e275388bf7c853aabddf52b68a39
welber.fernandes <welbih.ti@gmail.com>
2024-05-03 16:18:42 -0300

    Add lib icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,21 @@
-import { Container, Logo } from './styles';
+import { Container, Logo, BackIcon, BackButton } from './styles';
 
 import LogoImg from '@assets/logo.png';
 
-export function Header() {
+type Props = {
+    showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false}: Props) {
     return (
         <Container>
+            {
+                showBackButton &&
+                <BackButton>
+                    <BackIcon />
+                </BackButton>
+            }
+
             <Logo source={LogoImg}/>
         </Container>
     );
