d3428c29fae28205c591952f009a3050af286ae3
welber.fernandes <welbih.ti@gmail.com>
2024-05-03 14:47:57 -0300

    Add fonts

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-import { Container, Title } from './styles';
+import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
-      <Title>
-        Groups
-      </Title>
+
     </Container>
   );
 }
