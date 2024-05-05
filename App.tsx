c761ba6054ca3b134b4fe72894692ae881905530
welber.fernandes <welbih.ti@gmail.com>
2024-05-05 12:53:01 -0300

    structure navigation

--- App.tsx
+++ App.tsx
@@ -6,9 +6,8 @@ import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
 
-import { Groups } from '@screens/Groups';
-import { NewGroup } from '@screens/NewGroup';
-import { Players } from '@screens/Players';
+import { AppRoutes } from '@routes/app.routes';
+
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -20,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading />}
+      { fontsLoaded ? <AppRoutes /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { NavigationContainer } from '@react-navigation/native';
+
 import { Groups } from '@screens/Groups';
 import { NewGroup } from '@screens/NewGroup';
 import { Players } from '@screens/Players';
@@ -8,21 +10,23 @@ const  { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
     return (
-        <Navigator>
-            <Screen
-                name='groups'
-                component={Groups}
-            />
-            
-            <Screen
-                name='new'
-                component={NewGroup}
-            />
-            
-            <Screen
-                name='players'
-                component={Players}
-            />
-        </Navigator>
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false }}>
+                <Screen
+                    name='groups'
+                    component={Groups}
+                    />
+                
+                <Screen
+                    name='new'
+                    component={NewGroup}
+                    />
+                
+                <Screen
+                    name='players'
+                    component={Players}
+                    />
+            </Navigator>
+        </NavigationContainer>
     );
 }
