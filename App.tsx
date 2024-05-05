6525aa938effeba3ce8dc5835503b89f19c94397
welber.fernandes <welbih.ti@gmail.com>
2024-05-05 12:28:57 -0300

    install Stack Navigator

--- App.tsx
+++ App.tsx
@@ -20,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Players /> : <Loading />}
+      { fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
