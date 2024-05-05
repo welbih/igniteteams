be7b91193a24f4c52aac2cf0190ec2194efa3811
welber.fernandes <welbih.ti@gmail.com>
2024-05-05 14:38:44 -0300

    Implementing button back

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,8 +4,16 @@ import { Container, Content, Icon } from './styles';
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
+import { useNavigation } from '@react-navigation/native';
 
 export function NewGroup() {
+    
+    const navigation = useNavigation();
+
+    function handleNew() {
+        navigation.navigate('players', {group: 'Rocket'});
+    }
+    
     return (
         <Container>
             <Header showBackButton/>
@@ -23,6 +31,7 @@ export function NewGroup() {
                 <Button
                     title='Criar'
                     style={ {marginTop: 20}}
+                    onPress={handleNew}
                 />
             </Content>
 
